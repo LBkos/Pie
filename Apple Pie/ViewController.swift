c70c6c2fbc76a3238c8b02029593f82902fe4473
Konstantin <constantin.l.o.p@gmail.com>
2019-10-22 11:58:32 +0700

    Initial Commit

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Константин Лопаткин on 22.10.2019.
+//  Copyright © 2019 Konstantin Lopatkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
